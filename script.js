ca78f2f11b75d47eb88694c5eb45f913c6378801
NSHIMIYIMANA <joanesclaudio10@gmail.com>
2024-10-11 11:43:49 -0700

    email added

--- script.js
+++ script.js
@@ -7,9 +7,21 @@ document.addEventListener("DOMContentLoaded", () =>{
     form.addEventListener("submit", (event) =>{
       event.preventDefault()
 
-      const userName = document.getElementById("username").value.trim()
+      const username = document.getElementById("username").value.trim()
       const email = document.getElementById("email").value.trim();
       const password = document.getElementById("password").value.trim();
+      const isValid = true;
+      const messages = [];
+
+      if(username.length > 3){
+        const error1 ="please enter correct username"
+         messages.push(error1)
+      }
+
+      if(!email.includes("@") && !email.includes(".")){
+        const error2 = "Please enter valid email"
+       messages.push(error2);
+      }
 
     })
 })
