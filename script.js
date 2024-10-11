561492bc23ae27dbe13a28203fad0e25d707e800
NSHIMIYIMANA <joanesclaudio10@gmail.com>
2024-10-11 11:03:13 -0700

    HTML elements are gotten by DOM

---
+++ script.js
@@ -0,0 +1,6 @@
+const regForm = document.getElementById("registration-form");
+const userName = document.getElementById("username");
+const email = document.getElementById("email");
+const password = document.getElementById("password");
+const submitBtn = document.getElementById("submit");
+const feedback = document.getElementById("form-feedback");
